5740a53419fe8c845782b0e901299ab7a9232d38
feliposz <feliposz@gmail.com>
2024-08-19 01:43:33 -0300

    Write header section

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 )
@@ -29,14 +30,107 @@ func main() {
 		}
 
 		receivedData := string(buf[:size])
-		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
+		fmt.Printf("Received %d bytes from %s: %q\n", size, source, receivedData)
+
+		if size < 12 {
+			fmt.Println("Unexpected size:", size)
+			break
+		}
+
+		header := decodeDNSHeader([]byte(receivedData))
 
 		// Create an empty response
-		response := []byte{}
+		//response := []byte{}
+		header.QueryResponseIndicator = true
+		response := encodeDNSHeader(header)
 
 		_, err = udpConn.WriteToUDP(response, source)
 		if err != nil {
 			fmt.Println("Failed to send response:", err)
 		}
 	}
+}
+
+type DNSHeader struct {
+	PackedIdentifier       uint16
+	QuestionCount          uint16
+	AnswerRecordCount      uint16
+	AuthorityRecordCount   uint16
+	AdditionalRecordCount  uint16
+	QueryResponseIndicator bool
+	OperationCode          byte
+	AuthoritativeAnswer    bool
+	Truncation             bool
+	RecursionDesired       bool
+	RecursionAvailable     bool
+	CheckingDisabled       bool
+	AuthedData             bool
+	Z                      bool
+	ResponseCode           byte
+}
+
+func decodeDNSHeader(receivedData []byte) (header DNSHeader) {
+	header.PackedIdentifier = binary.BigEndian.Uint16(receivedData)
+	header.QuestionCount = binary.BigEndian.Uint16(receivedData[4:])
+	header.AnswerRecordCount = binary.BigEndian.Uint16(receivedData[6:])
+	header.AuthorityRecordCount = binary.BigEndian.Uint16(receivedData[8:])
+	header.AdditionalRecordCount = binary.BigEndian.Uint16(receivedData[10:])
+
+	flags := binary.BigEndian.Uint16(receivedData[2:])
+	a := byte(flags >> 8)
+	b := byte(flags & 0xFF)
+
+	header.RecursionDesired = (a & 0x01) != 0
+	header.Truncation = (a & 0x02) != 0
+	header.AuthoritativeAnswer = (a & 0x04) != 0
+	header.OperationCode = (a >> 3) & 0x0F
+	header.QueryResponseIndicator = (a & 0x80) != 0
+
+	header.ResponseCode = b & 0x0F
+	header.CheckingDisabled = (b & 0x10) != 0
+	header.AuthedData = (b & 0x20) != 0
+	header.Z = (b & 0x40) != 0
+	header.RecursionAvailable = (b & 0x80) != 0
+
+	return
+}
+
+func encodeDNSHeader(header DNSHeader) (response []byte) {
+	flags := uint16(0)
+	if header.RecursionDesired {
+		flags |= 0x0100
+	}
+	if header.Truncation {
+		flags |= 0x0200
+	}
+	if header.AuthoritativeAnswer {
+		flags |= 0x0400
+	}
+	if header.QueryResponseIndicator {
+		flags |= 0x8000
+	}
+	flags |= uint16(header.OperationCode) << 11
+	flags |= uint16(header.ResponseCode)
+
+	if header.CheckingDisabled {
+		flags |= 0x10
+	}
+	if header.AuthedData {
+		flags |= 0x20
+	}
+	if header.Z {
+		flags |= 0x40
+	}
+	if header.RecursionAvailable {
+		flags |= 0x80
+	}
+
+	response = binary.BigEndian.AppendUint16(response, header.PackedIdentifier)
+	response = binary.BigEndian.AppendUint16(response, flags)
+	response = binary.BigEndian.AppendUint16(response, header.QuestionCount)
+	response = binary.BigEndian.AppendUint16(response, header.AnswerRecordCount)
+	response = binary.BigEndian.AppendUint16(response, header.AuthorityRecordCount)
+	response = binary.BigEndian.AppendUint16(response, header.AdditionalRecordCount)
+
+	return
 }
---
+++ app/main_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDecodeDNSHeader(t *testing.T) {
+	data := []byte{0x86, 0x2a, 0x81, 0x80, 0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00}
+	header := decodeDNSHeader(data)
+	//fmt.Printf("%+v\n", header)
+	fmt.Println("PackedIdentifier: ", header.PackedIdentifier)
+	fmt.Println("QueryResponseIndicator: ", header.QueryResponseIndicator)
+	fmt.Println("OperationCode: ", header.OperationCode)
+	fmt.Println("AuthoritativeAnswer: ", header.AuthoritativeAnswer)
+	fmt.Println("Truncation: ", header.Truncation)
+	fmt.Println("RecursionDesired: ", header.RecursionDesired)
+	fmt.Println("RecursionAvailable: ", header.RecursionAvailable)
+	fmt.Println("CheckingDisabled: ", header.CheckingDisabled)
+	fmt.Println("AuthedData: ", header.AuthedData)
+	fmt.Println("Z: ", header.Z)
+	fmt.Println("ResponseCode: ", header.ResponseCode)
+	fmt.Println("QuestionCount: ", header.QuestionCount)
+	fmt.Println("AnswerRecordCount: ", header.AnswerRecordCount)
+	fmt.Println("AuthorityRecordCount: ", header.AuthorityRecordCount)
+	fmt.Println("AdditionalRecordCount: ", header.AdditionalRecordCount)
+}
+
+func TestEncodeDecode(t *testing.T) {
+	orig := DNSHeader{
+		PackedIdentifier:       34346,
+		RecursionDesired:       true,
+		Truncation:             false,
+		AuthoritativeAnswer:    false,
+		OperationCode:          0,
+		QueryResponseIndicator: true,
+		ResponseCode:           0,
+		CheckingDisabled:       false,
+		AuthedData:             false,
+		Z:                      false,
+		RecursionAvailable:     true,
+		QuestionCount:          1,
+		AnswerRecordCount:      1,
+		AuthorityRecordCount:   0,
+		AdditionalRecordCount:  0,
+	}
+	data := encodeDNSHeader(orig)
+	result := decodeDNSHeader(data)
+	if orig != result {
+		t.Fail()
+	}
+}
